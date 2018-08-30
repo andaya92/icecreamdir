ec1ed4a2a427836ada2f0bc26b87ffca91e2321f
andaya92 <andayac@gmail.com>
2018-08-30 15:42:48 -0700

    Allowed Host

--- myfriends/models.py
+++ myfriends/models.py
@@ -16,11 +16,4 @@ class User(AbstractUser):
 
 	def __str__(self):
 		return "{} ({})".format(self.username, self.id)
-	
-	@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-	def create_auth_token(sender, instance=None, created=False, **kwargs):
-		if created:
-			token = Token.objects.create(user=instance)
-			instance.token_user_auth = token.key
-			instance.save()
 
