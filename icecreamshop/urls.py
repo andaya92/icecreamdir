0454125e83188b4f207032481599d0c505505e8c
andaya92 <andayac@gmail.com>
2018-09-01 16:08:08 -0700

    home page

--- icecreamshop/urls.py
+++ icecreamshop/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from . import views
 
 urlpatterns = [
+    path('', views.Index.as_view()),
     path('admin/', admin.site.urls),
     path('directory/', include('directory.urls')),
     path('profile/', include('myprofile.urls')),
